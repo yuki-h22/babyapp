bd2621b878ee3dbe267d90e26eb3860e2fb3a31e
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-01 23:01:49 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,10 @@ module Babyapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    I18n.enforce_available_locales = true
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.2
-
+    config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
