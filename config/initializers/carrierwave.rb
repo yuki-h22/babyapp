83f41abd16cea780ea77b08d91fd2f34cb05481c
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-02 18:49:43 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :file
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = 'file/aws'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
